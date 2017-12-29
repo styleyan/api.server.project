5f257b1de086c6bb75b6720ec1c512623e5d6d4e
yanxiaofei <forget_love2161@126.com>
2017-12-30 01:41:17 +0800

    fix: 增加博客文章更新、删除功能

--- routes/www.isyxf.com/index.js
+++ routes/www.isyxf.com/index.js
@@ -18,6 +18,52 @@ module.exports = function (router) {
     }
   })
 
+  // 删除文章
+  router.post('/api/blog/delete/:articleId', async(ctx) => {
+    const data = ctx.params
+    const removeResult = await Article.remove({id: data.articleId})
+    let { result } = removeResult
+    let respon
+    if (result.ok && result.n) {
+      respon = {
+        msg: '',
+        code: '',
+        result: {},
+        status: 1
+      }
+    } else {
+      respon = {
+        msg: '服务器错误',
+        code: '10007',
+        result: {},
+        status: 0
+      }
+    }
+    ctx.body = respon
+  })
+
+  // 更新文章
+  router.post('/api/blog/update', async(ctx) => {
+    const data = ctx.request.body
+    const updateResult = await Article.update({id: data.id}, data)
+    let result
+    if (updateResult.ok && updateResult.n) {
+      result = {
+        status: 1,
+        msg: '',
+        code: '',
+        result: {},
+      }
+    } else {
+      result = {
+        status: 0,
+        msg: '更新失败，检查id是否正确',
+        code: '1008',
+      }
+    }
+    ctx.body = result
+  })
+
   // 查询文章列表
   router.post('/api/blog/list', async(ctx) => {
     const data = ctx.request.body
@@ -28,8 +74,8 @@ module.exports = function (router) {
     } else {
       skip = 0
     }
-
-    const docs = await Article.find({}, null, {skip, limit, sort: {createTime: 'desc'}}).lean().exec()
+    // TODO: 通过设置find方法第二个参数，排除返回的字段
+    const docs = await Article.find({}, {_id: false, __v: false, content: false}, {skip, limit, sort: {createTime: 'desc'}}).lean().exec()
     const count = await Article.count().exec()
     const result = {}
 
@@ -49,5 +95,16 @@ module.exports = function (router) {
     }
 
     ctx.body = result
+  })
+
+  // 查询文章详情
+  router.post('/api/blog/detail/:articleId', async(ctx) => {
+    const data = ctx.params
+    const result = await Article.findOne({id: data.articleId}, {_id: false, __v: false}).exec()
+    ctx.body = {
+      status: 1,
+      msg: '',
+      result,
+    }
   })
 }
