acf4a2c03f216ed02f61536f9f8c79c46126cd73
yanxiaofei <forget_love2161@126.com>
2019-05-04 23:43:45 +0800

    feat: 增加项目文件配置

---
+++ src/main/java/com/isyxf/blog/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.isyxf.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/out")
+    public String out() {
+        return "success";
+    }
+}
