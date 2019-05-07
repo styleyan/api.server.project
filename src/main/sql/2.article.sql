49acc0974f23335bbc8a850f0df1461becb006a7
yanxiaofei <forget_love2161@126.com>
2019-05-08 00:30:30 +0800

    feat: 修改sql语句防止出现外键创建失败

--- src/main/sql/2.article.sql
+++ src/main/sql/2.article.sql
@@ -1,13 +1,13 @@
 -- 创建文章表
 CREATE TABLE article(
-  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(10) AUTO_INCREMENT,
   `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `gtm_modify` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  `classify_id` bigint(10) NOT NULL COMMENT '专题分类id, 与专题表对应',
+  `classify_id` bigint(10) COMMENT '专题分类id, 与专题表对应',
   `title` varchar(20) NOT NULL COMMENT '文章标题',
   `desc` varchar(240) NOT NULL COMMENT '文章简介',
   `content` text NULL COMMENT '文章内容',
   `url` varchar(120) NOT NULL COMMENT '定义文章链接地址',
   `state` int NOT NULL DEFAULT 0 COMMENT '是否发布状态标识, 0: 隐藏, 1: 显示',
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='文章表';
+) ENGINE = InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='文章表';
--- src/main/sql/4.link.sql
+++ src/main/sql/4.link.sql
@@ -1,6 +1,6 @@
 -- 创建友链表
 CREATE TABLE link(
-   `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+   `id` bigint(10) unsigned AUTO_INCREMENT,
    `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
    `gtm_modify` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
    `website_name` varchar(20) NOT NULL COMMENT '友链名称',
@@ -8,6 +8,6 @@ CREATE TABLE link(
    `desc` varchar(240) NOT NULL COMMENT '友链描述',
    `https` int NOT NULL DEFAULT 1 COMMENT '是否是https: 0: 否, 1: 是',
    primary key (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='友链表';
+) ENGINE = InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8 COMMENT='友链表';
 
 
