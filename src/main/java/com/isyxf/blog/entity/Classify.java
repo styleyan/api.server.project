a80a18dc986d0a68763622797747eaf80f9f9cf5
yanxiaofei <forget_love2161@126.com>
2019-05-13 01:19:29 +0800

    feat: 增加 lombok

--- src/main/java/com/isyxf/blog/entity/Classify.java
+++ src/main/java/com/isyxf/blog/entity/Classify.java
@@ -1,66 +1,18 @@
 package com.isyxf.blog.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@ToString
 public class Classify {
-    private Integer id;
-
+    private long id;
     private Date gmtCreate;
-
     private Date gtmModify;
-
     private String desc;
-
     private String title;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGtmModify() {
-        return gtmModify;
-    }
-
-    public void setGtmModify(Date gtmModify) {
-        this.gtmModify = gtmModify;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Classify{" +
-                "id=" + id +
-                ", gmtCreate=" + gmtCreate +
-                ", gtmModify=" + gtmModify +
-                ", desc='" + desc + '\'' +
-                ", title='" + title + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/isyxf/blog/entity/Link.java
+++ src/main/java/com/isyxf/blog/entity/Link.java
@@ -1,88 +1,20 @@
 package com.isyxf.blog.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@ToString
 public class Link {
-    private Integer id;
-
+    private long id;
     private Date gmtCreate;
-
     private Date gtmModify;
-
     private String websiteName;
-
     private String websiteUrl;
-
     private String desc;
-
     private String https;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGtmModify() {
-        return gtmModify;
-    }
-
-    public void setGtmModify(Date gtmModify) {
-        this.gtmModify = gtmModify;
-    }
-
-    public String getWebsiteName() {
-        return websiteName;
-    }
-
-    public void setWebsiteName(String websiteName) {
-        this.websiteName = websiteName;
-    }
-
-    public String getWebsiteUrl() {
-        return websiteUrl;
-    }
-
-    public void setWebsiteUrl(String websiteUrl) {
-        this.websiteUrl = websiteUrl;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getHttps() {
-        return https;
-    }
-
-    public void setHttps(String https) {
-        this.https = https;
-    }
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "id=" + id +
-                ", gmtCreate=" + gmtCreate +
-                ", gtmModify=" + gtmModify +
-                ", websiteName='" + websiteName + '\'' +
-                ", websiteUrl='" + websiteUrl + '\'' +
-                ", desc='" + desc + '\'' +
-                ", https='" + https + '\'' +
-                '}';
-    }
 }
