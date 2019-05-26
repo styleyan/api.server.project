c67ed9984094eaa372e0ef9d7456711130e1971e
yanxiaofei <forget_love2161@126.com>
2019-05-26 23:48:41 +0800

    feat: 优化项目依赖

--- src/main/java/com/isyxf/blog/controller/MyController.java
+++ src/main/java/com/isyxf/blog/controller/MyController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/my")
 public class MyController {
     @RequestMapping("/out")
     public String out() {
