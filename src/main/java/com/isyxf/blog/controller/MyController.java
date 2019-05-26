ab4b9e8bd7642bd75b39d27c4c753ed457a90933
yanxiaofei <forget_love2161@126.com>
2019-05-27 00:46:05 +0800

    feat: 增加日志排查

--- src/main/java/com/isyxf/blog/controller/MyController.java
+++ src/main/java/com/isyxf/blog/controller/MyController.java
@@ -1,13 +1,16 @@
 package com.isyxf.blog.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/my")
+@Slf4j
 public class MyController {
     @RequestMapping("/out")
     public String out() {
+        log.info("===================我执行了");
         return "success";
     }
 }
