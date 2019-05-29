52504a5a3e5a6e7aff991bdfa07b75d83ee54687
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-05-29 16:59:29 +0800

    feat: 增加数据库查询功能

--- src/main/java/com/isyxf/blog/controller/MyController.java
+++ src/main/java/com/isyxf/blog/controller/MyController.java
@@ -2,10 +2,12 @@ package com.isyxf.blog.controller;
 
 import com.isyxf.blog.entity.Article;
 import com.isyxf.blog.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/my")
 public class MyController {
@@ -14,8 +16,13 @@ public class MyController {
 
     @RequestMapping("/out")
     public String out() {
-        Article article = articleService.getById(1);
+        Article article = null;
 
+        try {
+            article= articleService.getById(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         String str = "sdfsdf";
         return "success";
     }
