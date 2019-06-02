e5d54d208e2d6ac2254e9bd8f0c07e8febec5b05
yanxiaofei <forget_love2161@126.com>
2019-06-03 00:55:19 +0800

    feat: 增加添加文章接口

--- src/main/java/com/isyxf/blog/controller/ArticleController.java
+++ src/main/java/com/isyxf/blog/controller/ArticleController.java
@@ -4,10 +4,8 @@ import com.isyxf.blog.dto.GlobalResult;
 import com.isyxf.blog.entity.Article;
 import com.isyxf.blog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -16,6 +14,31 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json")
+    /**
+     * TODO: 使用 Article 对象来接收请求体, 这种形式会将 JSON 字符串的值赋予 user 中对应的属性上，需要注意的是
+     * JSON字符串中的key必须对应user中的属性名，否则是请求不过去的。
+     */
+    public void add(@RequestBody Article article) {
+        System.out.println(article);
+    }
+    /**
+     * 删除文章
+     */
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.DELETE)
+    public GlobalResult remove(@PathVariable("id") long id) {
+        GlobalResult globalResult;
+
+        try {
+            articleService.remove(id);
+            globalResult = new GlobalResult(true, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            globalResult = new GlobalResult(false, e.getMessage());
+        }
+
+        return globalResult;
+    }
     /**
      * 查询文章列表
      * @return List<Article> 返回文章列表
@@ -46,24 +69,5 @@ public class ArticleController {
         }
 
         return result;
-    }
-
-
-    /**
-     * 删除文章
-     */
-    @RequestMapping(value = "/{id}/delete", method = RequestMethod.DELETE)
-    public GlobalResult remove(@PathVariable("id") long id) {
-        GlobalResult globalResult;
-
-        try {
-            articleService.remove(id);
-            globalResult = new GlobalResult(true, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-            globalResult = new GlobalResult(false, e.getMessage());
-        }
-
-        return globalResult;
     }
 }
