e12ccfc43b6dcc08ab905a7d1641109e6a8902f5
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-09-16 14:48:04 +0800

    feat: 删除无用代码

--- server.config.js
+++ server.config.js
@@ -1,14 +1,6 @@
 const config = {
   // api服务器端口
   serverPort: 9090,
-  // mongo相关配置
-  mongo: {
-    host:'',
-    port: 27017,
-    db: '',
-    user: '',
-    pwd: '',
-  }
 }
 
 module.exports = config
