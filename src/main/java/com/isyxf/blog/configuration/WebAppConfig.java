150bb9260efe7ca487f2d6eced17b7f4222806a0
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-09-24 18:34:32 +0800

    feat: 修复增加了拦截器，application.properties 配置不生效问题

--- src/main/java/com/isyxf/blog/configuration/WebAppConfig.java
+++ src/main/java/com/isyxf/blog/configuration/WebAppConfig.java
@@ -7,16 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author xiaofei.yan
  * @Create 2019-09-16 11:24
  * @Descript 拦截器配置
+ * 最初 WebAppConfig extend WebMvcConfigurationSupport 出现jackson配置失效，才改为 WebAppConfig implements WebMvcConfigurer的
  */
 @Configuration
-public class WebAppConfig extends WebMvcConfigurationSupport {
+public class WebAppConfig implements WebMvcConfigurer {
     private final static Logger logger = LoggerFactory.getLogger(WebAppConfig.class);
+
 
     /**
      * 由于在拦截器中注解无效，需要提交注入 bean
@@ -27,7 +29,7 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
     }
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         /**
          * 所有以 api 开头的请求都需要经过 LoginHandler 校验
          * excludePathPatterns("", "", ...): 用于排查部分需要校验的氢气地址, 需要指定请求全路径
