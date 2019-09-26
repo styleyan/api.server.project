75fa4958df4aa9f7f0310ed08c0366317b0b1466
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-09-26 17:19:56 +0800

    feat: 内容页完善标签展示

--- src/main/java/com/isyxf/blog/service/impl/ClientServiceImpl.java
+++ src/main/java/com/isyxf/blog/service/impl/ClientServiceImpl.java
@@ -1,15 +1,20 @@
 package com.isyxf.blog.service.impl;
 
 import com.isyxf.blog.dao.ArticleDao;
+import com.isyxf.blog.dao.TagsDao;
 import com.isyxf.blog.dto.Result;
 import com.isyxf.blog.entity.Article;
 import com.isyxf.blog.service.ClientService;
+import com.isyxf.blog.utils.CommonUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Y.jer
@@ -21,6 +26,8 @@ public class ClientServiceImpl implements ClientService {
 
     @Resource
     private ArticleDao articleDao;
+    @Resource
+    private TagsDao tagsDao;
 
     @Override
     public Result archiveList() {
@@ -42,6 +49,22 @@ public class ClientServiceImpl implements ClientService {
     public Result articleDetail(String url) {
         try{
             Article article = articleDao.selectByUrl(url);
+            Map<Integer, String> tagList = CommonUtils.tagListToMap(tagsDao.selectAll());
+
+            String[] tags = article.getTags().split(",");
+            List<String> stringList = new ArrayList<>();
+
+            for (int j = 0; j < tags.length; j++) {
+                String curTag = tags[j];
+
+                if (StringUtils.isNotBlank(curTag)) {
+                    stringList.add(tagList.get(Integer.parseInt(curTag)));
+                }
+            }
+
+            // 将List 转字符串并加入分隔字符的方法
+            article.setTags(StringUtils.join(stringList.toArray(),","));
+
             return Result.success(article);
         } catch (Exception e) {
             e.printStackTrace();
